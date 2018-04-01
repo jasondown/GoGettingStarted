74ab4b5f4db1970fa705c5db656854ec5677e956
Jason Down <jdown@igloosoftware.com>
2018-04-01 01:19:46 -0400

    Added power grid report output. Added default option.

--- Module3/main.go
+++ Module3/main.go
@@ -7,6 +7,10 @@ import (
 func main() {
 	plantCapacities := []float64{30, 30, 30, 60, 60, 100}
 
+	activePlants := []int{0, 1}
+
+	var gridLoad = 75.
+
 	fmt.Println("1) Generate Power Plant Report")
 	fmt.Println("2) Generate Power Grid Report")
 	fmt.Println("Please choose and option: ")
@@ -19,5 +23,17 @@ func main() {
 		for idx, cap := range plantCapacities {
 			fmt.Printf("Plant %d capacity: %.0f\n", idx, cap)
 		}
+	case "2":
+		capacity := 0.
+		for _, plantID := range activePlants {
+			capacity += plantCapacities[plantID]
+		}
+
+		fmt.Printf("%-20s%.0f\n", "Capacity:", capacity)
+		fmt.Printf("%-20s%.0f\n", "Load:", gridLoad)
+		fmt.Printf("%-20s%.1f%%\n", "Utilization:", gridLoad/capacity*100)
+
+	default:
+		fmt.Println("Unknown option, no action taken")
 	}
 }
