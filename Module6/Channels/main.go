78131a646a424f3ae57c4a8898c42d4bf0e30be6
Jason Down <jdown@igloosoftware.com>
2018-04-03 23:57:17 -0400

    Added another channel to track when done messages rather than infinite loop and timer.

--- Module6/Channels/main.go
+++ Module6/Channels/main.go
@@ -2,29 +2,35 @@ package main
 
 import (
 	"runtime"
-	"time"
 )
 
 func main() {
 	runtime.GOMAXPROCS(8)
 
 	ch := make(chan string)
+	doneCh := make(chan bool)
 
 	go abcGen(ch)
-	go printer(ch)
+	go printer(ch, doneCh)
 
 	println("This comes first!")
-	time.Sleep(100 * time.Millisecond)
+
+	<-doneCh
+
 }
 
 func abcGen(ch chan string) {
 	for l := byte('a'); l <= byte('z'); l++ {
 		ch <- string(l)
 	}
+
+	close(ch)
 }
 
-func printer(ch chan string) {
-	for {
-		println(<-ch)
+func printer(ch chan string, doneCh chan bool) {
+	for l := range ch {
+		println(l)
 	}
+
+	doneCh <- true
 }
